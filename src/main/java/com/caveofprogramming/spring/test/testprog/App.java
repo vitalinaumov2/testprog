18849e99b3614479307fa6c74d8ea9ba32b36d3b
Vitali Naumov <vitali.naumov2@gmail.com>
2015-06-17 01:08:02 +0300

    Create maven project from maven-archetype-quickstart

---
+++ src/main/java/com/caveofprogramming/spring/test/testprog/App.java
@@ -0,0 +1,13 @@
+package com.caveofprogramming.spring.test.testprog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
